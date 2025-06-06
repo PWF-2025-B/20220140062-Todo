eb19d387b6524864056a55067f7e3a7e04f5c4b0
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-06-06 23:01:39 +0700

    api : auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,38 +6,25 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /** @Use HasFactory<\Database\Factories\UseFactory> */
+    use HasApiTokens,HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -49,5 +36,17 @@ class User extends Authenticatable implements MustVerifyEmail
     public function todos()
     {
         return $this->hasMany(Todo::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return[
+            'is_admin' => $this->is_admin,
+        ];
     }
 }
---
+++ tugas11.md
@@ -0,0 +1,4 @@
+# Tugas 11
+
+## 1. Browser Test Request
+![Alt text](screenshot/tugas11/test_auth_login.png)
